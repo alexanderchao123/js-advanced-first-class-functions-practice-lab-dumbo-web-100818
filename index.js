07e1a9118745e117a298aca6c4c89690088f2a0c
github username <chao.alexander.123@gmail.com>
2018-11-19 16:42:11 -0500

    define functions which utilize sort and reduce

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function(driver, i, arr) {
+    console.log(driver.name);
+  });
+};
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function(driver, i, arr) {
+    if (driver.hometown === location) {
+      console.log(driver.name);
+    };
+  });
+};
+
+function driversByRevenue(drivers) {
+  return drivers.slice(0).sort(function(first_driver, second_driver) {
+    return first_driver.revenue - second_driver.revenue;
+  });
+};
+
+function driversByName(drivers) {
+  return drivers.slice(0).sort(function(first_driver, second_driver) {
+    return first_driver.name.localeCompare(second_driver.name);
+  });
+};
+
+function totalRevenue(drivers){
+  return drivers.reduce(function(agg, driver, i, arr) {
+    return agg += driver.revenue;
+  }, 0);
+};
+
+function averageRevenue(drivers) {
+  const total = totalRevenue(drivers);
+  return total / drivers.length;
+}
